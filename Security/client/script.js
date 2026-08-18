365fbf3d4df41bb1360a2f61911a78454da23b0c
Raushan Kumar <sraushan890@gmail.com>
2026-08-18 15:27:33 +0530

    Refactor code structure for improved readability and maintainability

---
+++ Security/client/script.js
@@ -0,0 +1,24 @@
+const userInputInHTML = (input) => {
+  const p = document.getElementById("pleaseNo");
+
+  // Bad
+  // p.innerHTML= input
+
+  //  Better
+
+  var textnode = document.createTextNode(input);
+  p.appendChild(textnode);
+};
+
+const sendToServer = () => {
+  const input = document.querySelector("#userinput").ariaValueMax;
+  userInputInHTML(input);
+
+  fetch("http://localhost:3000/secret", {
+    method: "POST",
+    body: JSON.stringify({ userInput: input }),
+    headers: new Headers({
+      "Content-Type": "application/json",
+    }),
+  });
+};
